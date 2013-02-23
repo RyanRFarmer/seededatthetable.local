a5949b54792ccc4d2aba2bd1e07f05a4a1b34a9f
Ryan Farmer <studio258rf@gmail.com>
2013-02-22 19:30:25 -0600

    Final Launch Changes

--- wp-content/themes/seeded/footer.php
+++ wp-content/themes/seeded/footer.php
@@ -25,6 +25,18 @@ do_action( 'genesis_after_footer' );
 ?>
 
 <script type="text/javascript" src="/wp-content/themes/seeded/scripts/seeded.js"></script>
+<script type="text/javascript">
 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38735145-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </body>
 </html>
